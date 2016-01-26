eba04a63b552771b1acb3892545cfbca02625467
Paweł Urban <papcio28@gmail.com>
2016-01-26 19:51:50 +0100

    Metoda do wysylki SMSow.

--- smsmodule/src/main/java/pl/urban/android/lib/smsmodule/SmsUtil.java
+++ smsmodule/src/main/java/pl/urban/android/lib/smsmodule/SmsUtil.java
@@ -2,12 +2,14 @@ package pl.urban.android.lib.smsmodule;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.telephony.SmsManager;
 
 public final class SmsUtil {
     private SmsUtil() {
     }
 
-    public static void sendSMS(@NonNull Context context, @NonNull IncomingSmsBroadcastReceiver.SMSMessage message) {
-        // TODO: Wyslanie SMSa (multipart)
+    public static void sendSMS(@NonNull final Context context, @NonNull final IncomingSmsBroadcastReceiver.SMSMessage message) {
+        final SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendMultipartTextMessage(message.getNumber(), null, smsManager.divideMessage(message.getMessage()), null, null);
     }
 }
